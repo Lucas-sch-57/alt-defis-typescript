6a04d136dd83712b51666a12d38b66ba3f4bc903
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-15 09:38:07 +0200

    feat: write strLength function

--- src/string/string.ts
+++ src/string/string.ts
@@ -1,1 +1,15 @@
-console.log('ok')
+/**
+ * Treat a string to remove whitespace and return the number of chars
+ * @param string - The string treat
+ * @returns Should return the number of chars in the string without whitespace
+ */
+export const strLength = (string: string): number => {  
+    const result = string.replace(/\s/g,"").length
+    console.log(result)
+    return result
+}
+
+
+
+//Exemple 1 - Should return 15
+strLength("Bonjour le monde !")
